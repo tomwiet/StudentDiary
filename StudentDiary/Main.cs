0ee572ad6fadd8ac0dd64be36f5eb67e92a398e8
tomwiet <tthanquol@gmail.com>
2023-11-05 22:31:04 +0100

    Add refresh dgv in add and edit button

--- StudentDiary/Main.cs
+++ StudentDiary/Main.cs
@@ -48,7 +48,13 @@ namespace StudentDiary
         private void btnAdd_Click(object sender, EventArgs e)
         {
             var addEditStudent = new AddEditStudent();
+            addEditStudent.FormClosing += AddEditStudent_FormClosing;
             addEditStudent.ShowDialog();
+        }
+
+        private void AddEditStudent_FormClosing(object sender, FormClosingEventArgs e)
+        {
+           RefreshDiary();
         }
 
         private void btnEdit_Click(object sender, EventArgs e)
@@ -61,6 +67,7 @@ namespace StudentDiary
 
             var addEditStudent = new AddEditStudent(
                 Convert.ToInt32(dgvDiary.SelectedRows[0].Cells[0].Value));
+            addEditStudent.FormClosing += AddEditStudent_FormClosing;
             addEditStudent.ShowDialog();
         }
 
