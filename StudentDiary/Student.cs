387a8f2dcd0768d48ce292340aceaa2d304e8988
Tomasz Wieteska <tomasz.wieteska@elektrykplock.edu.pl>
2023-11-09 14:06:17 +0100

    Fix column position i datagridview

--- StudentDiary/Student.cs
+++ StudentDiary/Student.cs
@@ -11,14 +11,14 @@ namespace StudentDiary
         public int Id {  get; set; }    
         public string FirstName {  get; set; } 
         public string LastName { get; set; }
-        public string StudentGroup { get; set; }
+        public string studentGroupName { get; set; }
         public string Comments { get; set; }
         public string Math {  get; set; }
         public string Physics {  get; set; }
         public string Technology {  get; set; }
         public string PolishLang {  get; set; }
         public string ForeginLang {  get; set; }
-        public string studentGroupName {  get; set; }
+        
 
     }
 }
